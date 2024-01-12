2429740c95f70553c47d68c35d585d995c716ac6
Rassili <adnan.rassili47@gmail.com>
2024-01-12 17:23:00 +0100

    fixer error

--- views/detailwiki.php
+++ views/detailwiki.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
- session_start();
-if (!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 2) {
-    header('Location: login');
-    exit();
-}
+// require_once __DIR__ . '/../vendor/autoload.php';
+//  session_start();
+// if (!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 2) {
+//     header('Location: login');
+//     exit();
+// }
 // var_dump($_SESSION['user_id']);
 // exit();
 ?>
